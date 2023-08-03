dc4308c4160224f3e900e2de1dc0d8d00072b97d
firoz3130 <firosk7@gmail.com>
2023-08-03 21:38:54 +0530

    Completed the Basic level of fetching details and displaying data

--- weather.js
+++ weather.js
@@ -1,30 +1,46 @@
-const weatherForm = document.getElementById("weatherForm");
-weatherForm.addEventListener('submit',(event)=>{
-    event.preventDefault();
-    const weatherBtn=document.getElementById("getWeather");
-    const cityIns = document.getElementById("cityInput");
-    const city = cityIns.value;
-    console.log(cityIns);
-    console.log(city);
-    weatherBtn.addEventListener('click',()=>{
-        console.log("clicked");
-    });
+require('dotenv').config();
+const weatherForm = document.getElementById('weatherForm');
+const weatherApi = process.env.API_URL;
+const min_temperature = document.getElementsByClassName('min_temperature')[0];
+const max_temperature=document.getElementsByClassName('max_temperature')[0];
+const locationElement = document.getElementsByClassName('location')[0];
+const humidityElement = document.getElementsByClassName('humidity')[0];
+const windElement = document.getElementsByClassName('wind')[0];
+const apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+const currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+
+weatherForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const cityName = document.getElementById('cityInput').value;
+  const url = apiUrl + cityName + '&limit=1&appid=' + weatherApi;
+  const response = await fetch(url);
+  const json = await response.json();
+  const lat = json[0]['lat'];
+  const lon = json[0]['lon'];
+  const country=json[0]['country']
+  const state=json[0]['state']
+  const weatherUrl = currentWeatherUrl.replace('{lat}', lat).replace('{lon}', lon).replace('{API key}', weatherApi);
+  const currentResponse = await fetch(weatherUrl);
+  const currentJson = await currentResponse.json();
+  const whatWeatherNow = currentJson['weather'][0]['description'];
+  const temp = (currentJson['main']['temp'] - 273.15).toFixed(2);
+  const feels_like = (currentJson['main']['feels_like'] - 273.15).toFixed(2);
+  const temp_min = (currentJson['main']['temp_min'] - 273.15).toFixed(2);
+  const temp_max = (currentJson['main']['temp_max'] - 273.15).toFixed(2);
+  const pressure = currentJson['main']['pressure'];
+  const humidity = currentJson['main']['humidity'];
+  const wind = ((currentJson['wind']['speed']) * 1.852).toFixed(2);
+
+  min_temperature.innerHTML = "Min: " + temp_min + "°C";
+  max_temperature.innerHTML = "Max: " + temp_max + "°C";
+  locationElement.innerHTML = cityName+","+state+","+country;
+  humidityElement.innerHTML = "Humidity: " + humidity + "%";
+  windElement.innerHTML = "Wind: " + wind + " km/h";
+
+  console.log(whatWeatherNow, humidity, pressure, temp_max, temp_min);
+  console.log(feels_like + " and wind is " + wind + " km/hr");
+  console.log("country is ",country," ",temp);
+  console.log(json[0]['lat']);
+  console.log(json[0]['lon']);
+  console.log(cityName);
 });
-        
-        const counterElement = document.querySelector(".counter");
-
-        // Function to update the counter
-        const updateCounter = () => {
-            let seconds = 10000;
-
-            const countInterval = setInterval(() => {
-                seconds--;
-                counterElement.textContent = `${seconds} seconds`;
-
-                if (seconds <= 0) {
-                    clearInterval(countInterval);
-                    counterElement.textContent = "Time's up!";
-                }
-            }, 1000);
-        };
-        updateCounter();
